91fbfd7686019a42655aa49f158145f945291d23
patanmahaboobjan <patanmahaboobjan24@gmail.com>
2024-08-08 08:16:59 +0530

    Add files via upload

--- jaan.py
+++ jaan.py
@@ -1,10 +1,17 @@
-# This program adds two numbers
+# Python program to swap two variables
 
-num1 = 1.5
-num2 = 6.3
+x = 5
+y = 10
 
-# Add two numbers
-sum = num1 + num2
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
 
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+
